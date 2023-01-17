9a1be45d01371ae1b0332b61f52bde519aa57212
John Canero <johncanero.official@gmail.com>
2023-01-17 11:25:57 +0800

    Change Favicon to Upskill Hub Icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="Upskill Hub" />
         <meta property="og:description" content="Upskill Hub and Online Courses" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/upskillHubIcon.ico" />
 
         {/* URL */}
         {/* <meta property="og:url" content="https://johncanero.com/" /> */}
