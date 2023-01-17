1629e00e9eff4cea4836023fe8962eb126835a6a
John Canero <johncanero.official@gmail.com>
2023-01-17 17:13:24 +0800

    Apply Content on Footer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Poppins&display=optional:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic"
+            rel="stylesheet"
+          />
+
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+
+
+export default MyDocument;
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-6 bg-slate-900 md:px-36 font-Poppins">
 
-export default Footer
+      {/* Upskill and Get Started */}
+      <div className="md:justify-between md:flex">
+        {/* Title */}
+        <div>
+          <Link rel="noopener noreferrer" href="/">
+            <h1 className="hidden text-3xl font-bold text-white md:block">
+              upskillHub
+            </h1>
+          </Link>
+        </div>
+
+        <div className="">
+          <button className="w-48 h-12 font-bold text-white rounded-full bg-gradient-to-l from-purple-500 to-blue-500 hover:from-blue-500 hover:to-purple-500 ">
+            Get Started
+          </button>
+        </div>
+      </div>
+
+
+      {/* Copyright */}
+      <div className="pt-4 text-right text-gray-600 md:mt-1">
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/johncanero/"
+        >
+          <span className="mr-2 font-bold text-gray-300">
+            John Cañero
+          </span>
+        </Link>
+        &copy; {new Date().getFullYear()} All Rights Reserved
+      </div>
+
+
+
+    </footer>
+  );
+};
+
+export default Footer;
