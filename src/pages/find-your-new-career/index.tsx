4c11417e783d4f1d38b4446fd85bdee0aa09d003
John Canero <johncanero.official@gmail.com>
2023-01-18 08:59:19 +0800

    Change Font Styles of Pages to Open Sans

--- src/pages/find-your-new-career/index.tsx
+++ src/pages/find-your-new-career/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
     return (
         <div>
-            <h1 className="mt-6 text-2xl font-bold text-center font-Poppins">
+            <h1 className="mt-6 text-2xl font-bold text-center font-OpenSans">
                 Find Your New Career
             </h1>
         </div>
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
     return (
         <div>
-            <h1 className="mt-6 text-2xl font-bold text-center font-Poppins">
+            <h1 className="mt-6 text-2xl font-bold text-center font-OpenSans">
                 Profile
             </h1>
         </div>
