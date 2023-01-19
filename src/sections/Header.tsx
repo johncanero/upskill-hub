cdb96ebf9f2d5e34b6e3add56d42284fc142dce2
John Canero <johncanero.official@gmail.com>
2023-01-19 09:13:43 +0800

    Adjust Content of the Hero and Margin Padding

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -54,7 +54,7 @@ const Header = () => {
 
   return (
     <div>
-      <div className="hidden pt-3 lg:block lg:px-40">
+      <div className="hidden pt-3 lg:block lg:mx-52">
         <header className="flex items-center justify-between h-16 mt-1 md:mt-3">
 
           {/* Heading Title */}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,12 +7,14 @@ const Hero = () => {
     <div className=''>
       <div>
         {/* Description */}
-        <div className='ml-44'>
+        <div className='ml-52'>
           <h1 className='max-w-lg text-5xl font-bold mt-44'>Maximize skill, minimuze budget</h1>
+
+          <p className='max-w-xl'>Our modern courses across range of in-demand skills will give you the knowledge you need to live the life you want.</p>
         </div>
 
         {/* Image */}
-        <div className='relative lg:-mt-28'>
+        <div className='relative lg:-mt-44'>
           <Image
             src="/images/e-learningDesktop2-min.png"
             width={720}
