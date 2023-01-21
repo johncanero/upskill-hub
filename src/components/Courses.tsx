cc2aa541abc0b40b0e40d72a5ac740a8ad397c51
John Canero <johncanero.official@gmail.com>
2023-01-21 17:23:42 +0800

    Add Cards and Update the Feature adding Shadows

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -3,9 +3,70 @@ import React from 'react'
 const Courses = () => {
   return (
     <div>
-        <div className='mt-12 md:mt-72 lg:mx-64 b-36'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, vero quos? Exercitationem recusandae, nemo unde vitae labore quod inventore modi iusto, voluptatibus rem qui vero? Consequatur obcaecati ratione vel distinctio.
+
+      <div className='flex flex-wrap justify-center gap-10 mx-48 mt-72 mb-36'>
+
+        {/* Card */}
+        <div>
+          <div className="pt-10 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+            <div className="px-6 py-4">
+              <h2 className="mb-2 text-3xl font-bold w-52">Check out our most popular courses!</h2>
+            </div>
+          </div>
         </div>
+
+        {/* Card */}
+        <div>
+          <div className="pt-10 overflow-hidden text-gray-800 shadow-xl rounded-xl h-72 w-80 bg-slate-50 ">
+            <div className="px-6 py-4">
+              <h2 className="mb-2 text-3xl font-bold w-52">Check out our most popular courses!</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* Card */}
+        <div>
+          <div className="pt-10 overflow-hidden text-gray-800 shadow-xl bg-slate-50 rounded-xl h-72 w-80">
+            <div className="px-6 py-4">
+              <h2 className="mb-2 text-3xl font-bold w-52">Check out our most popular courses!</h2>
+            </div>
+          </div>
+        </div>
+
+
+        {/* Card */}
+        <div>
+          <div className="pt-10 overflow-hidden text-gray-800 shadow-xl rounded-xl h-72 w-80 bg-slate-50 ">
+            <div className="px-6 py-4">
+              <h2 className="mb-2 text-3xl font-bold w-52">Check out our most popular courses!</h2>
+            </div>
+          </div>
+        </div>
+
+        {/* Card */}
+        <div>
+          <div className="pt-10 overflow-hidden text-gray-800 shadow-xl rounded-xl h-72 w-80 bg-slate-50 ">
+            <div className="px-6 py-4">
+              <h2 className="mb-2 text-3xl font-bold w-52">Check out our most popular courses!</h2>
+            </div>
+          </div>
+        </div>
+
+
+        {/* Card */}
+        <div>
+          <div className="pt-10 overflow-hidden text-gray-800 shadow-xl bg-slate-50 rounded-xl h-72 w-80">
+            <div className="px-6 py-4">
+              <h2 className="mb-2 text-3xl font-bold w-52">Check out our most popular courses!</h2>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
     </div>
   )
 }
