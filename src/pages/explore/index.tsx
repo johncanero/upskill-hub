cafbd89b6bd74c0a577e399ed9acebefa120563f
John Canero <johncanero.official@gmail.com>
2023-01-26 13:05:16 +0800

    Adjust Width of Image for Unsplash

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -13,7 +13,7 @@ const index = () => {
                     width={1200}
                     height={1000}
                     alt="Explore Unsplash"
-                    className=" max-h-fit" />
+                    className="max-h-fit" />
             </div>
 
         </div>
