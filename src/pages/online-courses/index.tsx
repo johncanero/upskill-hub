dadfb7a7a2a779c7c9f60d2469e99abbf46c5f6e
John Canero <johncanero.official@gmail.com>
2023-01-27 00:33:43 +0800

    Add Courses Card

--- src/pages/online-courses/index.tsx
+++ src/pages/online-courses/index.tsx
@@ -6,6 +6,90 @@ const index = () => {
             <h1 className="mt-6 text-2xl font-bold text-center font-OpenSans">
                 Online Courses
             </h1>
+
+            <div className='flex flex-wrap justify-center gap-10 mt-8 lg:mx-56 mb-36'>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 1</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 2</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 3</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 4</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 5</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 6</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 7</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 8</h2>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Card */}
+                <div>
+                    <div className="pt-6 overflow-hidden text-white shadow-xl rounded-xl h-72 w-80 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div className="px-6 py-2">
+                            <h2 className="mb-2 text-3xl font-bold text-center font-OpenSans">Course 9</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
