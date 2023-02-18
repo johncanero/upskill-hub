99945589a58ef8848a3351d62fcb85b4263ab7f0
John Canero <johncanero.official@gmail.com>
2023-02-18 08:05:29 +0800

    Adjsut Gap and Turn the Get Started Link to About Name

--- src/sections/MobileFooter.tsx
+++ src/sections/MobileFooter.tsx
@@ -10,7 +10,7 @@ import { VscDebugStart } from "@react-icons/all-files/vsc/VscDebugStart"
 const MobileFooter = () => {
     return (
         <footer>
-            <div className="fixed bottom-0 left-0 flex justify-center w-full py-2 ml-1 text-center bg-gray-100 dark:bg-gray-900 border gap-6 dark:bg-black50 md:hidden ">
+            <div className="fixed bottom-0 left-0 flex justify-center w-full py-2 ml-1 text-center bg-gray-100 dark:bg-gray-900 gap-7 dark:bg-black50 md:hidden">
                 {/* Home */}
                 <Link href="/" rel="noopener noreferrer">
                     <button className="hover:text-neutral-700 dark:hover:text-gray-50">
@@ -30,8 +30,8 @@ const MobileFooter = () => {
                 {/* Get Started */}
                 <Link href="https://www.linkedin.com/company/upskill-hub/" rel="noopener noreferrer">
                     <button className="hover:text-neutral-700 dark:hover:text-gray-50">
-                        <VscDebugStart className="ml-5" size={22} />
-                        <p className="mt-1 text-xs text-center font-Poppins">Get Started</p>
+                        <VscDebugStart className="ml-1" size={22} />
+                        <p className="mt-1 text-xs text-center font-Poppins">About</p>
                     </button>
                 </Link>
 
