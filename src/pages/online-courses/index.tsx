90e9494b1e3250e00def48a81eea55390c89fa4f
John Canero <johncanero.official@gmail.com>
2023-03-19 12:08:33 +0800

    Add Dancing and Painting Feature for the Courses

--- src/pages/online-courses/index.tsx
+++ src/pages/online-courses/index.tsx
@@ -158,7 +158,7 @@ const index = () => {
                     </div>
                 </motion.div>
 
-                {/* Card 6 */}
+                {/* Card 7 */}
                 <motion.div
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
@@ -177,6 +177,54 @@ const index = () => {
                             />
                             <div className='flex justify-center mt-2'>
                                 <h2 className="mt-2 mb-2 font-bold text-gray-900 text-md font-OpenSans">Programming</h2>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+
+                {/* Card 8 */}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{
+                        duration: 0.8
+                    }}>
+                    <div className="w-full overflow-hidden text-white shadow-xl rounded-xl h-72 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div>
+                            {/* Image */}
+                            <Image
+                                src="/images/courses/dance-min.jpg"
+                                width={300}
+                                height={300}
+                                alt="Dancing"
+                                className="lg:block"
+                            />
+                            <div className='flex justify-center mt-2'>
+                                <h2 className="mt-2 mb-2 font-bold text-gray-900 text-md font-OpenSans">Dancing</h2>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+
+                {/* Card 9 */}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{
+                        duration: 0.8
+                    }}>
+                    <div className="w-full overflow-hidden text-white shadow-xl rounded-xl h-72 bg-gradient-to-l from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500">
+                        <div>
+                            {/* Image */}
+                            <Image
+                                src="/images/courses/painting-min.jpg"
+                                width={300}
+                                height={300}
+                                alt="Painting"
+                                className="lg:block"
+                            />
+                            <div className='flex justify-center mt-2'>
+                                <h2 className="mt-2 mb-2 font-bold text-gray-900 text-md font-OpenSans">Painting</h2>
                             </div>
                         </div>
                     </div>
