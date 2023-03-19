bc2a3acad905d2fe77a3d1fe21241c9d1864edaa
John Canero <johncanero.official@gmail.com>
2023-03-19 19:01:38 +0800

    Add Link to Browse Catalog

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -11,38 +11,39 @@ const index = () => {
                 Profile
             </h1> */}
             <div>
-                {/* Rectangle Background: Grow with Upskill Hub */}
+                {/* Component: Rectangle Background: Grow with Upskill Hub */}
                 <div className="md:px-12 px-6 lg:pl-64 h-44 md:h-52 bg-gradient-to-r from-cyan-600 to-blue-600 hover:from-blue-600 hover:to-cyan-600 text-black dark:text-white font-OpenSans">
                     <h2 className='md:pt-12 md:text-3xl text-2xl pt-8 lg:text-4xl font-bold'>Grow with Upskill Hub</h2>
                     <p className='lg:text-lg mt-1'>Learner help support: <Link
-                        rel="noopener noreferrer"
-                        href="https://upskillhub-by-johncanero.vercel.app/"
-                        className='hover:text-gray-800'>
-                        https://upskillhub-by-johncanero.vercel.app/
-                    </Link>
+                            rel="noopener noreferrer"
+                            href="https://upskillhub-by-johncanero.vercel.app/"
+                            className='hover:text-gray-800'>
+                            https://upskillhub-by-johncanero.vercel.app/
+                        </Link>
                     </p>
                 </div>
+
                 {/* Browse Catalog - My Courses - How it Works */}
-                <div className='md:px-12 px-6 lg:pl-64 h-16 shadow-lg pt-4 font-OpenSans dark:text-gray-400'>
+                <div className='md:px-12 px-6 lg:pl-64 h-16 shadow-lg pt-4 font-OpenSans dark:text-gray-400 md:text-base text-sm'>
                     <div className='flex justify-between'>
                         <div className='flex gap-5 md:gap-10'>
-                            <p>Browse Catalog</p>
+                            <Link
+                                rel="noopener noreferrer"
+                                href="/online-courses"
+                                className='hover:text-gray-200'>
+                                <p>Browse Catalog</p>
+                            </Link>
                             <p>My Courses</p>
                         </div>
                         <div className='lg:mr-56 font-semibold text-white'>
                             <div className='flex gap-2'>
                                 <p>How it Works</p>
-                                <BsChevronDown className='mt-1' size={16}/>
+                                <BsChevronDown className='mt-1' size={16} />
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
-            {/* 
-            <div className='flex justify-center mt-10'>
-                <CgProfile size={80} />
-            </div> */}
         </div>
     )
 }
