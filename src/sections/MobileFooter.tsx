0d0bf89361704af2f1ccc28d73730b8f9c3affd1
John Canero <johncanero.official@gmail.com>
2023-03-21 17:24:29 +0800

    Change Icon of About Page Link

--- src/sections/MobileFooter.tsx
+++ src/sections/MobileFooter.tsx
@@ -6,11 +6,12 @@ import { BiSearchAlt } from "@react-icons/all-files/bi/BiSearchAlt";
 import { GoLightBulb } from "@react-icons/all-files/go/GoLightBulb";
 import { GiNetworkBars } from "@react-icons/all-files/gi/GiNetworkBars"
 import { VscDebugStart } from "@react-icons/all-files/vsc/VscDebugStart"
+import { FcAbout } from "react-icons/fc"
 
 const MobileFooter = () => {
     return (
         <footer>
-            <div className="fixed bottom-0 left-0 flex justify-center w-full py-2 ml-1 text-center bg-gray-100 dark:bg-gray-900 gap-9 dark:bg-black50 md:hidden">
+            <div className="fixed bottom-0 left-0 flex justify-center w-full py-2 ml-1 text-center bg-gray-100 dark:bg-gray-900 gap-9 dark:bg-black50 md:hidden font-OpenSans">
                 {/* Home */}
                 <Link href="/" rel="noopener noreferrer">
                     <button className="hover:text-neutral-700 dark:hover:text-gray-50">
@@ -30,7 +31,7 @@ const MobileFooter = () => {
                 {/* Get Started */}
                 <Link href="https://www.linkedin.com/company/upskill-hub/" rel="noopener noreferrer">
                     <button className="hover:text-neutral-700 dark:hover:text-gray-50">
-                        <VscDebugStart className="ml-1" size={22} />
+                        <FcAbout className="ml-1" size={22} />
                         <p className="mt-1 text-xs text-center font-Poppins">About</p>
                     </button>
                 </Link>
