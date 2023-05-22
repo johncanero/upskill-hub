bcfcf0bbe5d84000c0bbbf2ebc04dabd78c155fe
John Canero <johncanero.official@gmail.com>
2023-05-22 17:57:57 +0800

    Add Image to Profile at Mobile Footer Icon

--- src/sections/MobileFooter.tsx
+++ src/sections/MobileFooter.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
 // Icons
 import { FaHome } from "@react-icons/all-files/fa/FaHome";
 import { BiSearchAlt } from "@react-icons/all-files/bi/BiSearchAlt";
 import { GoLightBulb } from "@react-icons/all-files/go/GoLightBulb";
-import { GiNetworkBars } from "@react-icons/all-files/gi/GiNetworkBars"
-import { VscDebugStart } from "@react-icons/all-files/vsc/VscDebugStart"
-import { FcAbout } from "react-icons/fc"
+import { GiNetworkBars } from "@react-icons/all-files/gi/GiNetworkBars";
+import { CgProfile } from "@react-icons/all-files/cg/CgProfile";
+// NextAuth
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const MobileFooter = () => {
+    const { data: session } = useSession();
+
     return (
         <footer>
             <div className="fixed bottom-0 left-0 flex justify-center w-full py-2 ml-1 text-center bg-gray-100 dark:bg-gray-900 gap-9 dark:bg-black50 md:hidden font-OpenSans">
@@ -28,11 +32,11 @@ const MobileFooter = () => {
                     </button>
                 </Link>
 
-                {/* Get Started */}
-                <Link href="https://www.linkedin.com/company/upskill-hub/" rel="noopener noreferrer">
+                {/* Online Courses */}
+                <Link href="/online-courses" rel="noopener noreferrer">
                     <button className="hover:text-neutral-700 dark:hover:text-gray-50">
-                        <FcAbout className="ml-1" size={22} />
-                        <p className="mt-1 text-xs text-center font-Poppins">About</p>
+                        <GoLightBulb className="ml-2" size={23} />
+                        <p className="mt-1 text-xs text-center font-Poppins">Courses</p>
                     </button>
                 </Link>
 
@@ -44,15 +48,24 @@ const MobileFooter = () => {
                     </button>
                 </Link>
 
-
-                {/* Online Courses */}
-                <Link href="/online-courses" rel="noopener noreferrer">
-                    <button className="hover:text-neutral-700 dark:hover:text-gray-50">
-                        <GoLightBulb className="ml-2" size={23} />
-                        <p className="mt-1 text-xs text-center font-Poppins">Courses</p>
-                    </button>
+                {/* Get Started */}
+                <Link href="/profile" rel="noopener noreferrer">
+                    {session ? (
+                        <button className="hover:text-neutral-700 dark:hover:text-gray-50">
+                            <img
+                                src={session.user?.image!}
+                                alt="user-avatar"
+                                className="w-6 mx-auto rounded-full cursor-pointer lg:mt-2 lg:mb-2 lg:mr-3 2xl:h-12 2xl:w-12 hover:opacity-50"
+                            />
+                            <p className="mt-1 text-xs text-center font-Poppins">Profile</p>
+                        </button>
+                    ) : (
+                        <button className="hover:text-neutral-700 dark:hover:text-gray-50">
+                            <CgProfile className="ml-1" size={23} />
+                            <p className="mt-1 text-xs text-center font-Poppins">Profile</p>
+                        </button>
+                    )}
                 </Link>
-
             </div>
         </footer>
     );
